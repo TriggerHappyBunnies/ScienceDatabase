6bc87ab1cd9bfaf4d04a0f5f84d3e22302b20f34
Aero <hegdeaakash@gmail.com>
2015-09-19 13:06:39 +0530

    Update ProjectMain.cpp

--- ProjectMain.cpp
+++ ProjectMain.cpp
@@ -14,7 +14,8 @@ using namespace std;
 class LSim
 {
 public:
-int osize,msize,isize,ipos,oposx,oposy,f=10;
+int v,oposx,f=10,u,R,h;
+double isize;
 char ch='a',ptr[5];
 
 int getf()
@@ -26,8 +27,19 @@ int getf()
 
 void DrawMir()
 {
+    setcolor(BROWN);
     arc((800+(f*2)*10),450,100,260,(f*2)*10);
+    setcolor(07);
     line(50,450,1550,450);
+    setcolor(LIGHTGRAY);
+    line(800-R/2,448,800-R/2,452);
+    line(800+R/2,448,800+R/2,452);
+    line(800-R,448,800-R,452);
+    line(800+R,448,800+R,452);
+}
+
+void Rays()
+{
 }
 
 int main()
@@ -40,12 +52,14 @@ int main()
         labelX:
         initwindow(1600,900);
         label:  
-        cleardevice();        
+        cleardevice();  
+        settextstyle(4,0,12);
+        setcolor(8);      
         outtextxy(200,100,"Focal Length :");
         outtextxy(200,800,"Press q to quit.");
         outtextxy(200,750,"Press f to change focal length.");
         itoa(f,ptr,10);
-        outtextxy(300,100,ptr);
+        outtextxy(375,100,ptr);
         if(kbhit())
         {    
             ch=getch();
@@ -65,9 +79,26 @@ int main()
         }
         else
         {
+            R=(f*2)*10;
             DrawMir();
             oposx=mousex();
+            u=oposx-800;
+            if(u>R/2-5&&u<R/2+5)
+            {    
+                outtextxy(200,150,"Image at infinity.");
+                setcolor(GREEN);
+                line(oposx,450,oposx,400);
+                delay(100);
+                goto label;
+            }
+            setcolor(GREEN);
             line(oposx,450,oposx,400);
+            v=(u*(R/2))/(u-(R/2));
+            isize=(double)v/u;
+            h=isize*50;
+            setcolor(LIGHTGREEN);
+            line(800+v,450,800+v,450+h);
+            //Rays();
             delay(100);
             goto label;
         }
@@ -75,4 +106,3 @@ int main()
     system("pause");
 }
 
-}
