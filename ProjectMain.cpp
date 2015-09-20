b00e2a84649e99bb4f1751c049952d3b0793131e
Aero <hegdeaakash@gmail.com>
2015-09-20 15:36:18 +0530

    Update ProjectMain.cpp

--- ProjectMain.cpp
+++ ProjectMain.cpp
@@ -14,7 +14,8 @@ using namespace std;
 class LSim
 {
 public:
-int v,oposx,f=10,u,R,h;
+int v,oposx=0,f=10,u,R,h;
+double p1,p;
 double isize;
 char ch='a',ptr[5];
 
@@ -40,6 +41,50 @@ void DrawMir()
 
 void Rays()
 {
+    p=sqrt(R*R-2500);
+    setcolor(WHITE);
+    line(oposx,400,800+R-p,400);//Parrallel ray to mirror
+    line(oposx,400,800,450);//Ray to pole
+    if(oposx<800&&f>0)
+    {
+        setcolor(WHITE);
+        line(800,450,oposx,500);//Reflected ray2
+        setcolor(DARKGRAY);
+        p1=(double)(1550-800)*h/v;
+        line(800,450,1550,450+p1);//Ray2 inside mirror
+        p1=(double)((400+(R/2))*100)/p;//HOW THE FK IS IT *100!!!! I FKING DID THIS FORMULA A MILLION TIMES AND THIS RANDOM VALUE FITS IN?!? 
+        setcolor(WHITE);
+        line(400,450-p1,800+R-p,400);//Reflected ray 1
+        setcolor(DARKGRAY);
+        line(800+R-p,400,800+(R/2),450);//Ray 1 inside mirror
+    }
+    if(oposx>800&&f>0)
+    {
+        if(oposx>800+(R/2))
+        {
+        setcolor(WHITE);
+        p1=(double)(1550-800)*h/v;
+        line(800,450,1550,450+p1);  //Reflected ray2
+        p1=(double)((1200-p)*50)/p;
+        setcolor(WHITE);
+        line(1400,450+p1,800+R-p,400);//Reflected ray 1 
+        }
+        else
+        {
+        setcolor(DARKGRAY);
+        p1=(double)((400+(R/2))*100)/p;
+        setcolor(DARKGRAY);
+        line(400,450-p1,800+R-p,400);
+        setcolor(WHITE);
+        line(800+R-p,400,800+(R/2),450);//Reflected ray 1
+        setcolor(WHITE);
+        p1=(double)(1550-800)*h/v;
+        line(800,450,1550,450+p1);  //Reflected ray2
+        setcolor(DARKGRAY);
+        line(800,450,50,450-p1);  //Ray 2 inside mirror
+        }
+    }
+        
 }
 
 int main()
@@ -52,7 +97,8 @@ int main()
         labelX:
         initwindow(1600,900);
         label:  
-        cleardevice();  
+        cleardevice();
+        labelY:   
         settextstyle(4,0,12);
         setcolor(8);      
         outtextxy(200,100,"Focal Length :");
@@ -77,10 +123,12 @@ int main()
             else
                 goto label;
         }
+         
         else
         {
+
             R=(f*2)*10;
-            DrawMir();
+            DrawMir();  
             oposx=mousex();
             u=oposx-800;
             if(u>R/2-5&&u<R/2+5)
@@ -98,13 +146,16 @@ int main()
             h=isize*50;
             setcolor(LIGHTGREEN);
             line(800+v,450,800+v,450+h);
-            //Rays();
-            delay(100);
+            Rays();  
+            delay(100);            
+            if(mousex()==oposx)
+                goto labelY;
             goto label;
         }
     }
     system("pause");
 }
+
 
 class compound_namer
 {
