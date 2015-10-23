dab850138fa0a05b9a65826b4123b00f7187288f
Aero <hegdeaakash@gmail.com>
2015-10-23 10:55:03 +0530

    Update ProjectMain.cpp

--- ProjectMain.cpp
+++ ProjectMain.cpp
@@ -5,18 +5,14 @@
 
 //I had coded without using a cass, so if u want to test it out on your PC just take the contents of the class
 //and dont take the class as a whole.
-//IT IS STILL INCOMPLTE. JUST A SAMPLE.
+//LIGHT SIMULATOR COMPLETE! Prone only to minor changes.
 #include<iostream>
-#include<graphics.h>
-using namespace std;
-
-class LS
-{
-    #include<iostream>
 #include<graphics.h>
 #include<math.h>
 using namespace std;
 
+class LS
+{
 int v,oposx=0,f=10,u,R,h;
 int x,y;
 double p1,p;
@@ -163,153 +159,6 @@ int main()
 }
 
 }
-
-
-class LSim
-{
-public:
-int v,oposx=0,f=10,u,R,h;
-double p1,p;
-double isize;
-char ch='a',ptr[5];
-
-int getf()
-{
-    cout<<"\nEnter new focal length : ";
-    cin>>f;
-    return f;
-}
-
-void DrawMir()
-{
-    setcolor(BROWN);
-    arc((800+(f*2)*10),450,100,260,(f*2)*10);
-    setcolor(07);
-    line(50,450,1550,450);
-    setcolor(LIGHTGRAY);
-    line(800-R/2,448,800-R/2,452);
-    line(800+R/2,448,800+R/2,452);
-    line(800-R,448,800-R,452);
-    line(800+R,448,800+R,452);
-}
-
-void Rays()
-{
-    p=sqrt(R*R-2500);
-    setcolor(WHITE);
-    line(oposx,400,800+R-p,400);//Parrallel ray to mirror
-    line(oposx,400,800,450);//Ray to pole
-    if(oposx<800&&f>0)
-    {
-        setcolor(WHITE);
-        line(800,450,oposx,500);//Reflected ray2
-        setcolor(DARKGRAY);
-        p1=(double)(1550-800)*h/v;
-        line(800,450,1550,450+p1);//Ray2 inside mirror
-        p1=(double)((400+(R/2))*100)/p;//HOW THE FK IS IT *100!!!! I FKING DID THIS FORMULA A MILLION TIMES AND THIS RANDOM VALUE FITS IN?!? 
-        setcolor(WHITE);
-        line(400,450-p1,800+R-p,400);//Reflected ray 1
-        setcolor(DARKGRAY);
-        line(800+R-p,400,800+(R/2),450);//Ray 1 inside mirror
-    }
-    if(oposx>800&&f>0)
-    {
-        if(oposx>800+(R/2))
-        {
-        setcolor(WHITE);
-        p1=(double)(1550-800)*h/v;
-        line(800,450,1550,450+p1);  //Reflected ray2
-        p1=(double)((1200-p)*50)/p;
-        setcolor(WHITE);
-        line(1400,450+p1,800+R-p,400);//Reflected ray 1 
-        }
-        else
-        {
-        setcolor(DARKGRAY);
-        p1=(double)((400+(R/2))*100)/p;
-        setcolor(DARKGRAY);
-        line(400,450-p1,800+R-p,400);
-        setcolor(WHITE);
-        line(800+R-p,400,800+(R/2),450);//Reflected ray 1
-        setcolor(WHITE);
-        p1=(double)(1550-800)*h/v;
-        line(800,450,1550,450+p1);  //Reflected ray2
-        setcolor(DARKGRAY);
-        line(800,450,50,450-p1);  //Ray 2 inside mirror
-        }
-    }
-        
-}
-
-int main()
-{
-    cout<<"Initial focal length is 10 units.\n";
-    system("pause");
-    initwindow(1600,900);
-    while(ch!='q')
-    {
-        labelX:
-        initwindow(1600,900);
-        label:  
-        cleardevice();
-        labelY:   
-        settextstyle(4,0,12);
-        setcolor(8);      
-        outtextxy(200,100,"Focal Length :");
-        outtextxy(200,800,"Press q to quit.");
-        outtextxy(200,750,"Press f to change focal length.");
-        itoa(f,ptr,10);
-        outtextxy(375,100,ptr);
-        if(kbhit())
-        {    
-            ch=getch();
-            if(ch=='q')
-            {
-                closegraph();
-                break;
-            }
-            else if(ch=='f')
-            {
-                closegraph();
-                f=getf();
-                goto labelX;
-            }
-            else
-                goto label;
-        }
-         
-        else
-        {
-
-            R=(f*2)*10;
-            DrawMir();  
-            oposx=mousex();
-            u=oposx-800;
-            if(u>R/2-5&&u<R/2+5)
-            {    
-                outtextxy(200,150,"Image at infinity.");
-                setcolor(GREEN);
-                line(oposx,450,oposx,400);
-                delay(100);
-                goto label;
-            }
-            setcolor(GREEN);
-            line(oposx,450,oposx,400);
-            v=(u*(R/2))/(u-(R/2));
-            isize=(double)v/u;
-            h=isize*50;
-            setcolor(LIGHTGREEN);
-            line(800+v,450,800+v,450+h);
-            Rays();  
-            delay(100);            
-            if(mousex()==oposx)
-                goto labelY;
-            goto label;
-        }
-    }
-    system("pause");
-}
-
 
 class compound_namer
 {
